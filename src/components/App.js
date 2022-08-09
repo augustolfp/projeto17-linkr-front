8faa02ada57407bc9d9fbb2346d5514374cf4145
Augusto Lopes <augustolfp@gmail.com>
2022-08-09 15:19:17 -0300

    Creates signIn, signUp and timeline components, and configures its routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import "../assets/styles/reset.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUpScreen from "./SignUpScreen/SignUpScreen";
+import TimeLineScreen from "./TimeLineScreen/TimeLineScreen";
+import SignInScreen from "./SignInScreen/SignInScreen";
 
 export default function App() {
     return(
-        <h1>Nosso Frontend</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/sign-up" element={<SignUpScreen />} />
+                <Route path="/timeline" element={<TimeLineScreen />} />
+                <Route path="/" element={<SignInScreen />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
---
+++ src/components/SignInScreen/SignInScreen.js
@@ -0,0 +1,5 @@
+export default function SignInScreen() {
+    return(
+        <h1>Sign in screen</h1>
+    );
+}
