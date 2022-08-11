5fac4b8b117a5e9834cfe46f80e527d442add0dd
Augusto Lopes <augustolfp@gmail.com>
2022-08-11 16:02:26 -0300

    Adds post creator component on timeline

--- src/components/PostBox/PostBox.js
+++ src/components/PostBox/PostBox.js
@@ -1,15 +1,38 @@
 import styled from "styled-components";
-
+import { useState } from "react";
 export default function PostBox() {
+    const [postUrl, setPostUrl] = useState("");
+    const [postDescription,setPostDescription] = useState("");
+    function publishPost(event) {
+        event.preventDefault();
+    }
+
     return(
         <PostCreatorContainer>
-            postBox
+            <h1>
+            What are you going to share today?
+            </h1>
+            <form onSubmit={publishPost}>
+                <input type="text" placeholder="url" onChange={e => setPostUrl(e.target.value)} required/>
+               <input type="text" placeholder="descrição" onChange={e => setPostDescription(e.target.value)} required/>
+               <button type="submit">PUBLISH</button>
+            </form>
         </PostCreatorContainer>
     );
 }
 
 const PostCreatorContainer = styled.div`
-    width: 300px;
+    width: 610px;
     height: 80px;
-    background-color: red;
+    background-color: white;
+    border-radius: 10px;
+    padding: 18px;
+
+    h1 {
+        font-family: 'Lato', sans-serif;
+        font-weight: 300;
+        color: #707070;
+        font-size: 20px;
+    }
 `
+
