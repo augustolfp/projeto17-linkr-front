1f4d2c369fca10c19b6a63e2298316323ff4505f
Augusto Lopes <augustolfp@gmail.com>
2022-08-12 14:38:32 -0300

    Adds PostBox styling

--- src/components/PostBox/PostBox.js
+++ src/components/PostBox/PostBox.js
@@ -1,29 +1,33 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import userDataContext from "../../contexts/userDataContext";
+
 export default function PostBox() {
     const [postUrl, setPostUrl] = useState("");
     const [postDescription,setPostDescription] = useState("");
+    const { userData, setUserData } = useContext(userDataContext);
     function publishPost(event) {
         event.preventDefault();
     }
 
     return(
         <PostCreatorContainer>
-            <h1>
-            What are you going to share today?
-            </h1>
-            <form onSubmit={publishPost}>
-                <input type="text" placeholder="url" onChange={e => setPostUrl(e.target.value)} required/>
-               <input type="text" placeholder="descrição" onChange={e => setPostDescription(e.target.value)} required/>
-               <button type="submit">PUBLISH</button>
-            </form>
+            <PerfilPhoto image={userData.pictureUrl}></PerfilPhoto>
+            <Container>
+                <h1>What are you going to share today?</h1>
+                <Form onSubmit={publishPost}> 
+                    <input type="text" placeholder="http://..." onChange={e => setPostUrl(e.target.value)} required/>
+                    <input type="text" placeholder="Awesome article about #javascript" onChange={e => setPostDescription(e.target.value)} required/>
+                    <button type="submit">Publish</button>
+                </Form>
+            </Container>
         </PostCreatorContainer>
     );
 }
 
 const PostCreatorContainer = styled.div`
-    width: 610px;
-    height: 80px;
+    display: flex;
+    width: 50vw;
     background-color: white;
     border-radius: 10px;
     padding: 18px;
@@ -36,3 +40,50 @@ const PostCreatorContainer = styled.div`
     }
 `
 
+const Container = styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+`
+
+const PerfilPhoto = styled.div`
+    border-radius: 50%;
+    background: ${props => props.image ? `url(${props.image})` : "white"};
+    background-position: center;
+    background-size: cover;
+    width: 40px;
+    height: 40px;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    width: 100%;
+
+    input {
+        background-color: #EFEFEF;
+        font-family: 'Lato', sans-serif;
+        font-weight: 300;
+        font-size: 15px;
+        border-style: none;
+        border-radius: 5px;
+        padding: 6px 14px;
+        margin: 6px 0px;
+        width: 100%;
+        box-sizing: border-box;
+    }
+
+    button {
+        background-color: #1877F2;
+        color: white;
+        font-family: 'Lato', sans-serif;
+        font-weight: 700;
+        border-style: none;
+        border-radius: 5px;
+        width: 112px;
+        height: 32px;
+
+    }
+`
